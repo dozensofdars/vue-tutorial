bb306a82574f8369ca1534899dbd73a7325d4280
dozensofdars <yuuya4000s@gmail.com>
2019-09-11 22:49:27 +0900

    v-if / v-else で編集機能作成

--- tutorial_vuejs_todo_management/src/components/HelloWorld.vue
+++ tutorial_vuejs_todo_management/src/components/HelloWorld.vue
@@ -9,7 +9,10 @@
     </form>
     <div class="task-list">
       <label class="task-list__item" v-for="todo in todos">
-        <input type="checkbox" v-model="todo.done"><button>EDIT</button>{{ todo.text }}
+        <input type="checkbox" v-model="todo.done">
+        <input type="checkbox" v-model="todo.editing">
+        <input v-if="todo.editing" type="text" v-model="todo.text" @keyup.enter="todo.editing = !todo.editing">
+        <span v-else>{{ todo.text }}</span>
       </label>
     </div>
   </div>
@@ -22,10 +25,10 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       todos: [
-        {text: "vue-router", done: false},
-        {text: 'vuex', done: false},
-        {text: 'vue-loader', done: false},
-        {text: 'awesome-vue', done: true },
+        {text: "vue-router", done: false, editing: false},
+        {text: 'vuex', done: false, editing: false},
+        {text: 'vue-loader', done: false, editing: false},
+        {text: 'awesome-vue', done: true, editing: false},
       ],
       newToDo: ""
     }
@@ -34,7 +37,7 @@ export default {
     addToDo: function(event) {
       let text = this.newToDo && this.newToDo.trim()
       if (!text) return
-      this.todos.push({text: text, done: false})
+      this.todos.push({text: text, done: false, editing: false})
       this.newToDo = ""
     },
     removeToDo: function(event) {
